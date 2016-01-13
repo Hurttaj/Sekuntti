9c3058af6f2ef675be4e6bce164598b3fd9d91c7
Jussi Hurtta <H9111@student.jamk.fi>
2016-01-13 13:24:35 +0200

    Hirveä ratkaisu.

---
+++ Sekuntti/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sekuntti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint Sec;
+            Console.WriteLine("Anna sekunttimäärä");
+            Sec = uint.Parse(Console.ReadLine());
+            uint T = (Sec / 3600);
+            uint M = ((Sec - (T * 3600)) / 60);
+            uint S = ((Sec - (T * 3600)) - (M * 60));
+            Console.WriteLine("Antamasi sekunttimäärä voidaan esittää myös muodossa " + T + " tuntia, " + M + " minuuttia ja " + S + " sekunttia.");
+
+        }
+    }
+}
+
+
